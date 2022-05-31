2625158d6b65d4d6e039fb7cab3c1d9c3f95c7f7
Alejo Boga <alejoboga20@gmail.com>
2022-05-31 10:10:37 -0300

    Tests for button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+	const [isPrimaryColor, setIsPrimaryColor] = useState(true);
+
+	const handleOnClick = () => setIsPrimaryColor(!isPrimaryColor);
+
 	return (
 		<div className='App'>
 			<div>
-				<button style={{ backgroundColor: 'red' }}>change to blue</button>
+				<button
+					style={{ backgroundColor: isPrimaryColor ? 'red' : 'blue' }}
+					onClick={handleOnClick}
+				>
+					{isPrimaryColor ? 'Change to Blue' : 'Change to Red'}
+				</button>
 			</div>
 		</div>
 	);
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import App from './App';
 
 test('button has correct initial color', async () => {
 	render(<App />);
-
 	const button = await screen.findByRole('button', { name: /change to blue/i });
+
 	expect(button).toHaveStyle({ backgroundColor: 'red' });
 });
 
-test('button has correct initial text', () => {});
+test('button has correct initial text', async () => {
+	render(<App />);
+	const button = await screen.findByRole('button', { name: /change to blue/i });
 
-test('button turns blue when clicked', () => {});
+	expect(button).toHaveTextContent('Change to Blue');
+});
+
+test('button turns blue when clicked and text changed', async () => {
+	render(<App />);
+	const button = await screen.findByRole('button', { name: /change to blue/i });
+
+	fireEvent.click(button);
+	expect(button).toHaveStyle({ backgroundColor: 'blue' });
+	expect(button).toHaveTextContent('Change to Red');
+});
